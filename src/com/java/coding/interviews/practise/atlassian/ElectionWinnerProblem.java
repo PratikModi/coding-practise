3772cb90fa69c0fb3a01b1cefaa88197475cd00a
Pratik Modi <Pratik.Modi@gmail.com>
2023-04-05 01:02:07 +0530

    New Coded Added

--- src/com/java/coding/interviews/practise/atlassian/ElectionWinnerProblem.java
+++ src/com/java/coding/interviews/practise/atlassian/ElectionWinnerProblem.java
@@ -1,7 +1,6 @@
 package com.java.coding.interviews.practise.atlassian;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Find winner of an election where votes are represented as candidate names
@@ -27,14 +26,27 @@ import java.util.Map;
 public class ElectionWinnerProblem {
     public static void main(String[] args) {
         String[] votes = {"john", "johnny", "jackie",
-                          "johnny", "john", "jackie",
-                          "jamie", "jamie", "john",
-                          "johnny", "jamie", "johnny",
-                          "john"};
+                "johnny", "john", "jackie",
+                "jamie", "jamie", "john",
+                "johnny", "jamie", "johnny",
+                "john"};
         System.out.println(findWinner(votes));
+
+        List<List<String>> votesList = new ArrayList<>();
+        List<String> vote1 = Arrays.asList("A","B","C");
+        List<String> vote2 = Arrays.asList("A","B","D");
+        List<String> vote3 = Arrays.asList("B","C","A");
+        votesList.add(vote1);
+        votesList.add(vote2);
+        votesList.add(vote3);
+
+        //System.out.println(findWinnerWithDifferentVoteWeight(votesList));
+        System.out.println(findWinnerWithDifferentStrategy(votesList));
     }
 
     public static String findWinner(String[] votes){
+        if(votes==null || votes.length==0)
+            return "NO WINNER";
         int highestVotesSoFar =0;
         String winner="";
         Map<String, Integer> election = new HashMap<>();
@@ -47,6 +59,75 @@ public class ElectionWinnerProblem {
                     highestVotesSoFar=noOfVotes;
                     winner = vote;
                 }
+            }
+        }
+        return winner;
+    }
+
+    public static String findWinnerWithDifferentVoteWeight(List<List<String>> votes){
+        if(votes==null || votes.isEmpty()){
+            return "NO WINNER";
+        }
+        int highestVotesSoFar=0;
+        String winner="";
+        Map<String,Integer> votesPerCandidate = new HashMap<>();
+        for(List<String> voteList : votes){
+            int voteWeight = votes.size();
+            for(String vote : voteList){
+                votesPerCandidate.putIfAbsent(vote,0);
+                votesPerCandidate.put(vote,votesPerCandidate.get(vote)+voteWeight);
+                int noOfVotes = votesPerCandidate.get(vote);
+                if(noOfVotes>highestVotesSoFar || (noOfVotes==highestVotesSoFar && winner.compareTo(vote)>0)){
+                    highestVotesSoFar=noOfVotes;
+                    winner=vote;
+                }
+                voteWeight--;
+            }
+        }
+        System.out.println(votesPerCandidate);
+        return winner;
+    }
+
+    public static String findWinnerWithDifferentStrategy(List<List<String>> votes){
+        if(votes==null || votes.isEmpty())
+            return "NO WINNER";
+        int highestVoteSoFar=0;
+        String winner="";
+        Map<String,Map<Integer,Integer>> votesPerCandidate = new HashMap<>();
+        for(List<String> voteList : votes){
+            int voteWeight=voteList.size();
+            int index=-1;
+            for(String vote : voteList){
+                index++;
+                votesPerCandidate.putIfAbsent(vote,new HashMap<>());
+                var voteMap = votesPerCandidate.get(vote);
+                voteMap.put(index,voteMap.getOrDefault(index,0)+voteWeight);
+                voteWeight--;
+            }
+
+        }
+        System.out.println(votesPerCandidate);
+        for(var entry : votesPerCandidate.entrySet()){
+            var candidate = entry.getKey();
+            int noOfVotes = entry.getValue().values().stream().reduce((a,b)->a+b).get();
+            if(noOfVotes>=highestVoteSoFar){
+                if(noOfVotes>highestVoteSoFar){
+                    highestVoteSoFar=noOfVotes;
+                    winner=candidate;
+                }else{
+                    var oldWinner = votesPerCandidate.get(winner);
+                    var newWinner = votesPerCandidate.get(candidate);
+                    for(int i=0;i<votes.size();i++){
+                        int oldVotes = oldWinner.getOrDefault(i,0);
+                        int newVotes = newWinner.getOrDefault(i,0);
+                        if(oldVotes!=newVotes && newVotes>oldVotes){
+                            highestVoteSoFar= newVotes;
+                            winner=candidate;
+                            break;
+                        }
+                    }
+                }
+
             }
         }
         return winner;
