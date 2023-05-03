f4958ea498f3c08f9e7a9963e4c79ecd2ce5461a
Pratik Modi <Pratik.Modi@gmail.com>
2023-05-03 16:50:55 +0530

    New Coded Added

--- src/com/java/coding/interviews/practise/jpmc/ReverseLinkedListProblem.java
+++ src/com/java/coding/interviews/practise/jpmc/ReverseLinkedListProblem.java
@@ -3,11 +3,26 @@ package com.java.coding.interviews.practise.jpmc;
 public class ReverseLinkedListProblem {
 
     public static void main(String[] args) {
+        Node singly = new Node(1);
+        singly.next = new Node(2);
+        singly.next.next = new Node(3);
+        singly.next.next.next = new Node(4);
+        System.out.println(reverseSingly(singly));
 
+        Node doubly = new Node(1);
+        doubly.next = new Node(2);
+        doubly.previous=null;
+        doubly.next.next = new Node(3);
+        doubly.next.previous = doubly.next;
+        doubly.next.next.next = new Node(4);
+        doubly.next.next.previous = doubly.next.next;
+        System.out.println(doubly);
+        reverseDoubly(doubly);
+        System.out.println(doubly);
     }
 
     public static Node reverseSingly(Node head){
-        Node next=null;
+        Node next;
         Node current=head;
         Node previous=null;
         while(current!=null){
@@ -17,6 +32,21 @@ public class ReverseLinkedListProblem {
             current=next;
         }
         return previous;
+    }
+
+    public static void reverseDoubly(Node head){
+        Node temp=null;
+        Node current=head;
+        while(current!=null){
+            temp = current.previous;
+            current.previous = current.next;
+            current.next=temp;
+            current=current.previous;
+        }
+        System.out.println(temp);
+        if(temp!=null){
+            head = temp.previous;
+        }
     }
 
 }
