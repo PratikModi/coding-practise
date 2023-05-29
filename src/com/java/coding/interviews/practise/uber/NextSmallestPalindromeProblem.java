9db5d0aa0ce81212f67aba0fcb0773aa3a641896
Pratik Modi <Pratik.Modi@gmail.com>
2023-05-29 23:40:04 +0530

    New Coded Added

--- src/com/java/coding/interviews/practise/uber/NextSmallestPalindromeProblem.java
+++ src/com/java/coding/interviews/practise/uber/NextSmallestPalindromeProblem.java
@@ -43,5 +43,68 @@ package com.java.coding.interviews.practise.uber;
  */
 public class NextSmallestPalindromeProblem {
 
+    public static void main(String[] args) {
+        System.out.println(findNextPalindrome("199"));
+        System.out.println(findNextPalindrome("2456"));
+        System.out.println(findNextPalindrome("1"));
+        System.out.println(findNextPalindrome("12345"));
+    }
+
+    private static String addOne(String S){
+        return String.valueOf(Integer.parseInt(S)+1);
+    }
+
+    private static String reverse(String S){
+        return new StringBuilder(S).reverse().toString();
+    }
+
+    private static boolean isGreater(String left, String right){
+        return Integer.parseInt(left) > Integer.parseInt(right);
+    }
+
+    public static String findNextPalindrome(String S){
+        if(S==null || S.length()==0)
+            return S;
+        if(S.length()==1){
+            if(S.equalsIgnoreCase("9")) return "11";
+            else return addOne(S);
+        }
+        String SS="";
+        if(S.equalsIgnoreCase(reverse(S))){
+            SS=addOne(S);
+        }else{
+            SS=S;
+        }
+        int N = SS.length();
+        int mid=0;
+        if(N%2==0){
+            mid=N/2;
+            String left = SS.substring(0,mid);
+            String right = SS.substring(mid);
+            if(isGreater(reverse(left),right)){
+                return left + reverse(left);
+            }else{
+                String newLeft = addOne(left);
+                return newLeft+reverse(newLeft);
+            }
+        }else{
+            mid=N/2;
+            String left = SS.substring(0,mid);
+            String right = SS.substring(mid+1);
+            if(isGreater(reverse(left),right)){
+                return left+S.charAt(mid)+reverse(left);
+            }else{
+                if(SS.charAt(mid)!='9'){
+                    char c = (char) (((SS.charAt(mid) - '0') + 1) + '0');
+                    return left+c+reverse(left);
+                }else{
+                    String newLeft = addOne(left+SS.charAt(mid));
+                    left = newLeft.substring(0,mid);
+                    return newLeft+reverse(left);
+                }
+            }
+        }
+    }
+
 
 }
