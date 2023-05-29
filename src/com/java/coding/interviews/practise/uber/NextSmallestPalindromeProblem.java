d291152b26fee002166c36af57180756585bf160
Pratik1 <pratik.modi@gmail.com>
2023-05-29 18:54:50 +0530

    New Code Added

---
+++ src/com/java/coding/interviews/practise/uber/NextSmallestPalindromeProblem.java
@@ -0,0 +1,47 @@
+package com.java.coding.interviews.practise.uber;
+
+/**
+ * Problem Description
+ *
+ * Given a numeric string A representing a large number you need to find the next smallest palindrome greater than this number.
+ *
+ *
+ *
+ * Problem Constraints
+ * 1 <= |A| <= 100
+ *
+ * A doesn't start with zeroes and always contain digits from 0-9.
+ *
+ *
+ *
+ * Input Format
+ * First and only argument is an string A.
+ *
+ *
+ *
+ * Output Format
+ * Return a numeric string denoting the next smallest palindrome greater than A.
+ *
+ *
+ *
+ * Example Input
+ * Input 1:
+ *
+ *  A = "23545"
+ * Input 2:
+ *
+ *  A = "999"
+ *
+ *
+ * Example Output
+ * Output 1:
+ *
+ *  "23632"
+ * Output 2:
+ *
+ *  "1001"
+ */
+public class NextSmallestPalindromeProblem {
+
+
+}
